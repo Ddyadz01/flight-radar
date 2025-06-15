feb7ccb631c14cf723ea5e70d89971aacdb7686d
Ddyadz01 <albertmik2002@gmail.com>
2025-06-15 16:24:57 +0300

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ const App = () => {
             {/*<Polyline positions={route} color="gray" weight={2} dashArray="1" />*/}
             <TileLayer
                 url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-
+                attribution='&copy; <a href="https://carto.com/">CARTO</a>'
             />
 
 
